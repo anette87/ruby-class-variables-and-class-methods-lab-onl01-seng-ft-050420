8a7e7cef030d1a3e7982c5161a0e30d411c7937c
Anette Rivera <anettem.rivera@gmail.com>
2020-05-14 05:03:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+
+@@count = 0  
+@@genres = []
+
+attr_accessor :name, :artist, :genre 
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres_count 
+  end
+  
+end
